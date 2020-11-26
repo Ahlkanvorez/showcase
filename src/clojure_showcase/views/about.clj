5db665ce34451dd4e57c8bb92a743684bd6c9c69
Robert Mitchell <robert.mitchell36@gmail.com>
2020-11-25 19:36:11 -0600

    Add work history to the about page

--- src/clojure_showcase/views/about.clj
+++ src/clojure_showcase/views/about.clj
@@ -1,8 +1,34 @@
 (ns clojure-showcase.views.about
-  (:require [clojure-showcase.views.article :as article]))
+  (:require [clojure-showcase.utils :as utils]
+            [clojure-showcase.views.article :as article]
+            [clojure-showcase.views.layout :as layout]))
+
+(defn role-content [role]
+  [:div {:class "p-2 d-flex flex-row"}
+   [:h5 {:class "h5 mr-auto"} (:title role)]
+   [:span (:dates role)]])
+
+(defn work-history-for [job]
+  [:div {:class "jumbotron"}
+   [:h4 {:class "h4"} (:title job)]
+   [:div {:class "d-flex flex-column"}
+    (for [role (:roles job)]
+      (role-content role))]
+   (:description job)])
+
+(defn work-history []
+  [:div {:class "container-fluid bg-light"}
+   [:h4 {:class "display-4"}
+    "Work History"]
+   (for [job (utils/work-history)]
+     (work-history-for job))])
 
 (defn content []
-  (article/content "about"))
+  [:div
+   (article/content "about")
+   (work-history)])
 
 (defn view []
-  (article/view "about"))
+  (layout/base
+   :title (str "Robert Mitchell | About")
+   :content (content)))
